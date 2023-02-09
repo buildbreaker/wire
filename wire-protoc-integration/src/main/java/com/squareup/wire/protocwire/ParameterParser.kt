42086fc48a0cda4c1f4751a5baa147e46e2b3842
Alan Chiu <awlchiu@gmail.com>
2023-02-09 10:52:41 -0800

    comment

--- wire-protoc-integration/src/main/java/com/squareup/wire/protocwire/ParameterParser.kt
+++ wire-protoc-integration/src/main/java/com/squareup/wire/protocwire/ParameterParser.kt
@@ -1,8 +1,17 @@
 package com.squareup.wire.protocwire
 
-class ParameterParser {
+internal class ParameterParser {
   companion object {
-    fun parse(parameter: String): Map<String, String> {
+    /**
+     * Parses protoc parameters.
+     * Protoc parameters are the `opt` passed as a single string.
+     *
+     * Wire targets have configuration which relies on multiple values.
+     * In order to preserve that behavior, the opts which are passed through
+     * will be assumed to be key value pairs separated by `=` and
+     * different pairs are delimited by `,`.
+     */
+    internal fun parse(parameter: String): Map<String, String> {
       val parsedParameters = mutableMapOf<String, String>()
       val split = parameter.split(',')
       split.forEach { str -> str.trim() }
