6c76548c4f7bb2a1642b981f2e0ff68c965f8298
Alan Chiu <awlchiu@gmail.com>
2023-02-10 11:25:44 -0800

    fixup

--- wire-protoc-integration/src/main/java/com/squareup/wire/protocwire/WireGenerator.kt
+++ wire-protoc-integration/src/main/java/com/squareup/wire/protocwire/WireGenerator.kt
@@ -122,10 +122,19 @@ class WireGenerator(
 }
 
 class FileSystem(private val response: Plugin.Response): ForwardingFileSystem(SYSTEM) {
+  /**
+   * Returns a single sink per file from protoc.
+   *
+   * It isn't guaranteed that flush() is called prior to close().
+   * The behavior intends to address this issue by calling flush()
+   * on close() if it hasn't been called yet. The flush() method
+   * is the canonical way to write to file.
+   */
   override fun sink(file: Path, mustCreate: Boolean): Sink {
     return object: Sink {
       private val buffer = Buffer()
       private var isFlushed = false
+
       override fun close() {
         flush()
         buffer.close()
